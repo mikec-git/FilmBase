50a4b9ccad6bdd498959809d93f52d233831f25b
Mike Choi <mike.choi@live.ca>
2018-12-23 13:08:15 -0800

    Added meta description tag

--- frontend/src/store/initStore.js
+++ frontend/src/store/initStore.js
@@ -1,12 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index';
 import rootSaga from './sagas/index';
 
 export default function initStore() {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const sagaMiddleware  = createSagaMiddleware();
-  const middleWare      = composeEnhancers(applyMiddleware(sagaMiddleware));
+  const middleWare      = applyMiddleware(sagaMiddleware);
   const store           = createStore(rootReducer, middleWare);
 
   sagaMiddleware.run(rootSaga);
